bdc64a7f36fc05c0e1c9a2b52293090b685a2711
Yazid Kherrati <Yazidkherrati@gmail/com>
2019-09-18 10:38:36 +0200

    Page connexion version 0.1

--- app/src/main/java/com/example/sharehit/LoginPage.java
+++ app/src/main/java/com/example/sharehit/LoginPage.java
@@ -1,11 +1,27 @@
 package com.example.sharehit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginPage extends AppCompatActivity {
+
+    private EditText emailLog, passLog;
+    private Button loginButt;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +29,32 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        mAuth = FirebaseAuth.getInstance();
+        emailLog = (EditText) findViewById(R.id.emailLog);
+        passLog = (EditText) findViewById(R.id.passLog);
+        loginButt = (Button) findViewById(R.id.loginButt);
+
+        loginButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = emailLog.getText().toString();
+                final String password = passLog.getText().toString();
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginPage.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                               if(!task.isSuccessful()){
+                                   Toast.makeText(LoginPage.this, "Authentication failed." + task.getException(), Toast.LENGTH_LONG).show();
+                               }else {
+                                   startActivity(new Intent(LoginPage.this, MainActivity.class));
+                                   finish();
+                               }
+                            }
+                        });
+
+            }
+        });
     }
 }
