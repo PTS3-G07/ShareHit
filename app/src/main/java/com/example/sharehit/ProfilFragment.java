f7af12b7082e984e563540fa4d6f1f4315e06d82
Leo Brunet <leobrunet91@gmail.com>
2019-11-30 15:53:57 +0100

    test img

--- app/src/main/java/com/example/sharehit/ProfilFragment.java
+++ app/src/main/java/com/example/sharehit/ProfilFragment.java
@@ -59,7 +59,11 @@ import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
@@ -371,6 +375,12 @@ public class ProfilFragment extends Fragment {
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.getData() != null) {
             imguri = data.getData();
+            /*File compressedImageFile = new File(imguri.getPath());
+            try {
+                OutputStream os = new FileOutputStream(compressedImageFile);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }*/
             final StorageReference filepath = mStorageRef.child(user.getUid());
             filepath.putFile(imguri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
                 @Override
